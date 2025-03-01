19ccc6f1945e231106692139064ae00ef1b37c75
Jose Azuara <joseag312@gmail.com>
2025-03-01 15:17:47 -0800

    Test commit

--- ComponentsC/ScaleComponent.cs
+++ ComponentsC/ScaleComponent.cs
@@ -5,7 +5,7 @@ public partial class ScaleComponent : Node
 {
 	[Export] Node2D sprite;
 	[Export] Vector2 scaleAmount = new Vector2(1.5f, 1.5f);
-	[Export] float scaleDuration = 0.4f;
+	[Export] float scaleDuration = 0.5f;
 
 	public void TweenScale()
 	{
