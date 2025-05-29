78c287a4c389594f5dfe50865ddacdea2ef5e320
Jose Azuara <joseag312@gmail.com>
2025-05-29 07:05:54 -0700

    RoadMap and Currency autoload changes

--- autoload/GameStats.cs
+++ autoload/GameStats.cs
@@ -4,8 +4,10 @@ public partial class GameStats : Node
 {
 	public static GameStats Instance { get; private set; }
 
-	[Export] public int Score { get; set; } = 0;
-	[Export] public int HighScore { get; set; } = 0;
+	[Export] public int Pawllars { get; set; } = 0;
+	[Export] public int Mewnits { get; set; } = 0;
+
+	private const string SavePath = "user://savegame_game.dat";
 
 	public override void _Ready()
 	{
@@ -18,5 +20,32 @@ public partial class GameStats : Node
 		{
 			QueueFree();
 		}
+	}
+
+	public void Save()
+	{
+		var data = new Godot.Collections.Dictionary<string, Variant>
+		{
+			{ "pawllars", Pawllars },
+			{ "mewnits", Mewnits }
+		};
+
+		using var file = FileAccess.Open(SavePath, FileAccess.ModeFlags.Write);
+		file.StoreVar(data);
+	}
+
+	public void Load()
+	{
+		if (!FileAccess.FileExists(SavePath))
+		{
+			Save();
+			return;
+		}
+
+		using var file = FileAccess.Open(SavePath, FileAccess.ModeFlags.Read);
+		var data = (Godot.Collections.Dictionary)file.GetVar();
+
+		Pawllars = (int)data["pawllars"];
+		Mewnits = (int)data["mewnits"];
 	}
 }
--- autoload/ShipStats.cs
+++ autoload/ShipStats.cs
@@ -7,6 +7,8 @@ public partial class ShipStats : Node
 	[Export] public int Health { get; set; } = 25;
 	[Export] public int Speed { get; set; } = 250;
 
+	private const string SavePath = "user://savegame_ship.dat";
+
 	public override void _Ready()
 	{
 		if (Instance == null)
@@ -18,5 +20,32 @@ public partial class ShipStats : Node
 		{
 			QueueFree();
 		}
+	}
+
+	public void Save()
+	{
+		var data = new Godot.Collections.Dictionary<string, Variant>
+		{
+			{ "health", Health },
+			{ "speed", Speed }
+		};
+
+		using var file = FileAccess.Open(SavePath, FileAccess.ModeFlags.Write);
+		file.StoreVar(data);
+	}
+
+	public void Load()
+	{
+		if (!FileAccess.FileExists(SavePath))
+		{
+			Save();
+			return;
+		}
+
+		using var file = FileAccess.Open(SavePath, FileAccess.ModeFlags.Read);
+		var data = (Godot.Collections.Dictionary)file.GetVar();
+
+		Health = (int)data["health"];
+		Speed = (int)data["speed"];
 	}
 }
